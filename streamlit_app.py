d3ef1390436c63973b20cbf034244b3e47beb642
sebvachon <svachon@e-vachon.com>
2022-06-02 12:45:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,11 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
+streamlit.header('Breakfast Menu')
+
+streamlit.title('Oatmeal')
+
+streamlit.title('Kale')
+
+streamlit.title('Eggs')
 
