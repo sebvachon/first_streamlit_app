132b5a734f298740e915f3e045dff0b57c2e996d
sebvachon <svachon@e-vachon.com>
2022-06-02 12:46:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-
-streamlit.text('Oatmeal')
-
-streamlit.text('Kale')
-
-streamlit.text('Eggs')
-
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
