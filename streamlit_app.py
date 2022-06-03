94ce15cbcbcdaee1541eaf43460b449193f5c858
sebvachon <svachon@e-vachon.com>
2022-06-03 19:23:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,8 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado'
 streamlit.dataframe(my_fruit_list)
 
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write("Ok! searcing for ", fruit_choice)
 #streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
